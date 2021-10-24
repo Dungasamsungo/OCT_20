3d2bd8aaa26b40fb575d00873d6a1f9272374d7a
sampson <sampsonngobi@gmail.com>
2021-10-24 16:01:21 -0300

    We are now able to reset student`s name through input

--- OCT20/Main.java
+++ OCT20/Main.java
@@ -1,23 +1,33 @@
 package OCT20;
 import java.time.LocalDate;
+
+import javax.swing.JOptionPane;
 public class Main {
     
     public static void main(String[] args) {
 
         LocalDate toDay = LocalDate.now();
-              System.out.println("Curent Date: " + toDay);
-           Human student1 = new Human("Sam", 1988, 8.9);
-           student1.setName("John");
+              
 
-           System.out.println("Student's Details");
+           Human student1 = new Human("Sam", 1988, 8.9);
+               student1.setName(JOptionPane.showInputDialog(null, "Enter Name"));
+               int dateOfBirth = Integer.parseInt( student1.setDateOfBirth(JOptionPane.showInputDialog(null, "Enter Date of birth")));
+
+
+
+           
            System.out.println(); student1.printdetails(); 
 
-           Human student2 = new Human("Sam", 1992, 6.7);
+           
+           
+           
+           
+           /*Human student2 = new Human("Sam", 1992, 6.7);
               student2.setName("Micheal");
               student2.setDateOfBirth(1934);
               student2.setHeight(9.4);
 
-            System.out.println();student2.printdetails();
+            System.out.println();student2.printdetails();*/
              
        
         
